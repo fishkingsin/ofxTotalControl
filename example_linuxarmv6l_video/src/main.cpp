34bb2baa5ce06cb003357d8e8307c77c45edcf8d
fishkingsin <fishkingsin@gmail.com>
2013-02-04 13:29:29 +0800

    updated exmaple_linuxarmv6l_video

--- example_linuxarmv6l_video/src/main.cpp
+++ example_linuxarmv6l_video/src/main.cpp
@@ -1,15 +1,15 @@
 #include "ofMain.h"
-#ifdef TARGET_LINUX_ARM
-#include "ofGLES2Renderer.h"
-#endif
+//#ifdef TARGET_LINUX_ARM
+//#include "ofGLES2Renderer.h"
+//#endif
 #include "testApp.h"
 
 //========================================================================
 int main( ){
-#ifdef TARGET_LINUX_ARM
-	ofGLES2Renderer * renderer = new ofGLES2Renderer();
-	ofSetCurrentRenderer(ofPtr<ofBaseRenderer>(renderer));
-#endif
+//#ifdef TARGET_LINUX_ARM
+//	ofGLES2Renderer * renderer = new ofGLES2Renderer();
+//	ofSetCurrentRenderer(ofPtr<ofBaseRenderer>(renderer));
+//#endif
 	ofSetupOpenGL(100,100, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
